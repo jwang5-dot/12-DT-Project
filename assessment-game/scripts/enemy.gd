6af173145f055a9be31b68b12c997aa08a538375
Max Chee <90257046+MaxIsBetterThanYou@users.noreply.github.com>
2026-06-26 14:47:02 +1200

    Screen Recording for Prototype

--- assessment-game/scripts/enemy.gd
+++ assessment-game/scripts/enemy.gd
@@ -4,34 +4,28 @@ extends CharacterBody2D
 var speed: float = 200.0
 var player: CharacterBody2D
 var health: int = 100
+
+@export var health_ui: ProgressBar
 		
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for node in get_tree().get_nodes_in_group("player"):
 		player = node
 
-@export var health_ui: ProgressBar
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#if not player == null:
-		#look_at(player.global_position)
-		#velocity = Vector2(1, 0).rotated(rotation) * speed
-		#move_and_slide()
+func _process(delta: float) -> void:
+	if not player == null:
+		look_at(player.global_position)
+		velocity = Vector2(1, 0).rotated(rotation) * speed
+		move_and_slide()
 
 func take_damage() -> void:
 	if health > 1:
 		health -= 5
 		health_ui.value = health
-	else:
-		player.add_score(1)  
+	else: 
 		queue_free()
-
-func _damage_player(body: Node2D) -> void:
-	if body == Player_1:
-		body.take_damage()
-
 
 func _take_damage(body: Node2D) -> void:
 	if body is Player_1:
