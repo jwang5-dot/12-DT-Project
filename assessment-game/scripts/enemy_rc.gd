92273819a4f3f8d7bd5dee591d5a10e276731130
Max Chee <90257046+MaxIsBetterThanYou@users.noreply.github.com>
2026-07-24 14:07:21 +1200

    script for enemy fireball

--- assessment-game/scripts/enemy_rc.gd
+++ assessment-game/scripts/enemy_rc.gd
@@ -7,6 +7,11 @@ var health: int = 100
 
 @export var health_ui: ProgressBar
 @export var sprite: Sprite2D
+
+# Shooting
+@export var bullet_scene: PackedScene
+@export var bullet_spawn: Marker2D
+var can_shoot: bool = true
 		
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -24,8 +29,13 @@ func _process(delta: float) -> void:
 		look_at(player.global_position)
 		velocity = Vector2(1, 0).rotated(rotation) * speed
 		move_and_slide()
+
+		if can_shoot:
+			_shoot()
+
 	health_ui.get_parent().rotation = -rotation
 	sprite.global_rotation = 0
+
 
 func take_damage() -> void:
 	if health > 1:
@@ -34,6 +44,24 @@ func take_damage() -> void:
 	else: 
 		queue_free()
 
+
 func _take_damage(body: Node2D) -> void:
 	if body is Player_1:
 		body.take_damage()
+
+
+func _shoot() -> void:
+	if bullet_scene == null or bullet_spawn == null:
+		return
+
+	var bullet = bullet_scene.instantiate()
+
+	bullet.global_position = bullet_spawn.global_position
+	bullet.rotation = rotation
+
+	get_parent().add_child(bullet)
+
+	can_shoot = false
+
+	await get_tree().create_timer(2.0).timeout
+	can_shoot = true
