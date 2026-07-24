e6be18106a037034dfe96b5168c1096613b435cf
Max Chee <90257046+MaxIsBetterThanYou@users.noreply.github.com>
2026-07-24 14:01:09 +1200

    Enemy Fireball

---
+++ assessment-game/enemy_bullet.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+var speed: float = 1500
+
+func _process(delta: float) -> void:
+	move_local_x(speed * delta)
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("enemy"):
+		body.take_damage()
+		queue_free()
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
