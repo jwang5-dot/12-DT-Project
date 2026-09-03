ff6b52d14fd3c3eb16f84aded14cb3c12a135c7f
Max Chee <90257046+MaxIsBetterThanYou@users.noreply.github.com>
2026-09-04 10:44:23 +1200

    Main Menu changes

--- assessment-game/scripts/mainmenu.gd
+++ assessment-game/scripts/mainmenu.gd
@@ -11,3 +11,7 @@ func _quit() -> void:
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scene/controls.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene/credits.tscn")
